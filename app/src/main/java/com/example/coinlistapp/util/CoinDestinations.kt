945c93e74878435ce442a9e4dbf05d6ea3de733f
jason <readonly23@gmail.com>
2024-05-06 11:51:19 +0100

    Added data class CoinListState

---
+++ app/src/main/java/com/example/coinlistapp/util/CoinDestinations.kt
@@ -0,0 +1,20 @@
+package com.example.coinlistapp.util
+
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+interface CoinDestination {
+    val route: String
+}
+
+object Home : CoinDestination {
+    override val route = "home"
+}
+
+object CoinDetail : CoinDestination {
+    override val route = "coin_detail"
+    const val coinIdArg = "coin_id"
+    val routeWithArgs = "$route/{$coinIdArg}"
+    val arguments = listOf(navArgument(coinIdArg) { type = NavType.StringType }
+    )
+}
