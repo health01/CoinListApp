88a87d667178f6a6537cbf95d1c125f87d83949a
jason <readonly23@gmail.com>
2024-05-06 11:13:49 +0100

    Added class CoinRepository and CoinRepositoryImpl

---
+++ app/src/main/java/com/example/coinlistapp/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.example.coinlistapp.data.repository
+
+import com.example.coinlistapp.data.dto.Coin
+import com.example.coinlistapp.data.dto.CoinDetail
+import com.example.coinlistapp.data.network.CoinAPiService
+import com.example.coinlistapp.repository.CoinRepository
+import javax.inject.Inject
+
+class CoinRepositoryImpl @Inject constructor(
+    private val apiService: CoinAPiService
+) : CoinRepository {
+    override suspend fun getCoins(): List<Coin> {
+        return apiService.getCoins()
+    }
+
+    override suspend fun getCoinsDetail(coinId: String): CoinDetail {
+        return apiService.getCoinDetail(coinId)
+    }
+}
---
+++ app/src/main/java/com/example/coinlistapp/repository/CoinRepository.kt
@@ -0,0 +1,10 @@
+package com.example.coinlistapp.repository
+
+import com.example.coinlistapp.data.dto.Coin
+import com.example.coinlistapp.data.dto.CoinDetail
+
+
+interface CoinRepository {
+    suspend fun getCoins(): List<Coin>
+    suspend fun getCoinsDetail(coinId: String): CoinDetail
+}
