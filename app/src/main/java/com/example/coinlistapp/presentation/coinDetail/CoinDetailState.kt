35297bb870eac33e2d567c1c15fe53c2012f0bcc
jason <readonly23@gmail.com>
2024-05-06 11:57:21 +0100

    Added CoinDetailState and CoinDetailViewModel

---
+++ app/src/main/java/com/example/coinlistapp/presentation/coinDetail/CoinDetailState.kt
@@ -0,0 +1,10 @@
+package com.example.coinlistapp.presentation.coinDetail
+
+import com.example.coinlistapp.data.DataState
+import com.example.coinlistapp.data.dto.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail? = null,
+    val error: DataState.Error? = null
+)
---
+++ app/src/main/java/com/example/coinlistapp/presentation/coinDetail/CoinDetailViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.coinlistapp.presentation.coinDetail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.coinlistapp.data.DataState
+import com.example.coinlistapp.domain.repository.usecase.CoinUseCase
+import com.example.coinlistapp.util.CoinDetail
+import com.example.coinlistapp.util.DefaultCoroutineDispatchers
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinDetailViewModel @Inject constructor(
+    private val coinUseCase: CoinUseCase,
+    savedStateHandle: SavedStateHandle,
+    private val dispatchers: DefaultCoroutineDispatchers,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinDetailState())
+    val state: StateFlow<CoinDetailState> = _state
+
+    init {
+        try {
+            savedStateHandle.get<String>(CoinDetail.coinIdArg)?.let { coinId ->
+                getCoinDetail(coinId)
+            }
+
+        } catch (throwable: Throwable) {
+            _state.value = _state.value.copy(
+                isLoading = false,
+                error = DataState.Error(null, throwable.message)
+            )
+        }
+    }
+
+    fun getCoinDetail(coinId: String) {
+        viewModelScope.launch(dispatchers.io()) {
+            coinUseCase.getCoinById(coinId).collect { result ->
+                when (result) {
+                    is DataState.Loading -> {
+                        _state.value = _state.value.copy(isLoading = true)
+                    }
+
+                    is DataState.Error -> {
+                        _state.value = CoinDetailState(error = result)
+                    }
+
+                    is DataState.Success -> {
+                        _state.value = _state.value.copy(isLoading = false, coin = result.data)
+                    }
+                }
+            }
+        }
+    }
+}
