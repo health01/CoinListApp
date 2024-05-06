50a6a91ffa76e0e983e72ceedec00df0948c5b6e
jason <readonly23@gmail.com>
2024-05-06 11:55:13 +0100

    Added CoinListViewModel

---
+++ app/src/main/java/com/example/coinlistapp/presentation/coinList/CoinListViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.coinlistapp.presentation.coinList
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.coinlistapp.data.DataState
+import com.example.coinlistapp.domain.repository.usecase.CoinUseCase
+import com.example.coinlistapp.util.DefaultCoroutineDispatchers
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CoinListViewModel @Inject constructor(
+    private val coinUseCase: CoinUseCase,
+    private val dispatchers: DefaultCoroutineDispatchers,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(CoinListState())
+    val state: StateFlow<CoinListState> = _state
+
+    init {
+        getCoinList()
+    }
+
+    fun getCoinList() {
+        viewModelScope.launch(dispatchers.main()) {
+
+            coinUseCase.getCoinList().collect { result ->
+                when (result) {
+                    is DataState.Loading -> {
+                        _state.value = _state.value.copy(isLoading = true)
+                    }
+
+                    is DataState.Error -> {
+                        _state.value = CoinListState(error = result)
+                    }
+
+                    is DataState.Success -> {
+                        _state.value = CoinListState(coins = result.data)
+                    }
+                }
+            }
+        }
+    }
+}
